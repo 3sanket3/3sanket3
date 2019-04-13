52f398878ee04eaf41799573816f31c60252baa3
Sanket Patel <3sanket3@gmail.com>
2019-04-13 12:53:26 +0530

    replace tools to side projects and remove codesandbox

--- config.js
+++ config.js
@@ -1,39 +1,39 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Sanket Patel\'s Website',
-  subtitle: 'A fullstack developer',
-  disqusShortname: '',
+  url: "https://lumen.netlify.com",
+  title: "Sanket Patel's Website",
+  subtitle: "A fullstack developer",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about"
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: "Contact me",
+      path: "/pages/contacts"
     },
     {
-      label: 'Tools',
-      path: '/pages/tools'
+      label: "Side Projects",
+      path: "/pages/side-projects"
     }
   ],
   author: {
-    name: 'Sanket Patel',
-    photo: '/photo.jpg',
-    bio: 'A fullstack web developer👨‍💻, Father👨, Indian 🇮🇳, BAPS follower 🙏',
+    name: "Sanket Patel",
+    photo: "/photo.jpg",
+    bio: "A fullstack web developer👨‍💻, Father👨, Indian 🇮🇳, BAPS follower 🙏",
     contacts: {
-      email: '3sanket3@gmail.com',
-      twitter: '3sanket3',
-      github: '3sanket3',
-      resume: 'https://novoresume.com/a/sanket.1'
+      email: "3sanket3@gmail.com",
+      twitter: "3sanket3",
+      github: "3sanket3",
+      resume: "https://novoresume.com/a/sanket.1"
     }
   }
 };
---
+++ content/pages/side-projects.md
@@ -0,0 +1,8 @@
+---
+title: "Side Projects"
+template: "page"
+---
+
+## Slugify the text
+
+[https://slugify.netlify.com/](https://slugify.netlify.com/)
