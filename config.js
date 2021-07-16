0952bd5261af3735d2587d81f810edccf05d1a12
Sanket Patel <3sanket3@gmail.com>
2021-07-16 12:56:06 +0530

    update about

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ module.exports = {
     name: "Sanket Patel",
     photo: "/photo.jpg",
     bio:
-      "CEO at https://redsoftware.in/, Father👨, Indian 🇮🇳, BAPS Follower 🙏",
+      "CEO at RedSoft Solutions Pvt. Ltd. - redsoftware.in, Father👨, Indian 🇮🇳, BAPS Follower 🙏",
     contacts: {
       email: "hi@3sanket3.com",
       twitter: "3sanket3",
--- content/pages/contacts.md
+++ content/pages/contacts.md
@@ -5,6 +5,6 @@ template: "page"
 
 ## Say hi
 
-- [twitter](https://www.twitter.com/3sanket3)
+- [Twitter](https://www.twitter.com/3sanket3)
 - [LinkedIn](https://www.linkedin.com/in/3sanket3/)
 - [sanket@redsoftware.in](mailto:sanket@redsoftware.in)
